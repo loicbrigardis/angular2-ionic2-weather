a588bccd398c85f73d07b1978a3c91902f022b03
loic <loic.brigardis@gmail.com>
2016-07-16 18:53:02 +0200

    weather page update

--- app/pages/weather/weather.ts
+++ app/pages/weather/weather.ts
@@ -7,12 +7,22 @@ import {WeatherService} from '../../services/weather.services';
     providers: [WeatherService]
 })
 export class WeatherPage implements OnInit {
+    
+    city = 'Boston';
+    state = 'MA'
+    weather;
+    
     constructor(
         private navController: NavController,
         private _weatherService: WeatherService
         ) { }
 
     ngOnInit() { 
+        this._weatherService.getWeather(this.city, this.state)
+        .subscribe(w => {
+            console.log(w);
+            this.weather = w;
+        });
         
     }
 
--- app/services/weather.services.ts
+++ app/services/weather.services.ts
@@ -6,9 +6,17 @@ import 'rxjs/Rx';
 @Injectable()
 export class WeatherService {
 
-    constructor(private _http:Http) { 
-        console.log('Service connected...');
-        
+    private _apiKey: string = "60557eb5d5b9e65e";
+    private _apiUrl: string = "http://api.wunderground.com/api/" + this._apiKey + "/conditions/q/";
+
+
+    constructor(private _http: Http) {
+    }
+
+    getWeather (city, state) {
+        return this._http.get(this._apiUrl + state +'/'+ city + '.json')
+        .map (x => x.json());
+    
     }
 
 }
